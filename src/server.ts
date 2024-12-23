e8866d0d7ecddb180f702339457aba452ff91576
MATEAV23 <mateogalvarezvic23@gmail.com>
2024-12-23 16:18:02 -0300

    configurando variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import colors from 'colors'
 import express from 'express'
 import router from './router'
 import db from './config/db'
@@ -8,10 +9,10 @@ async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log('Conexion exitosa a la base de datos')
+        console.log( colors.blue.bold('Conexion exitosa a la base de datos') )
     } catch (error) {
-        console.log(error)
-        console.log('Hubo un error al conectar a la Base de Datos')
+        // console.log(error)
+        console.log(colors.red.bold('Hubo un error al conectar a la Base de Datos'))
     }
 }
 
