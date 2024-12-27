4e818f5f31f78d2f21f084465a117542c84fa4b1
MATEAV23 <mateogalvarezvic23@gmail.com>
2024-12-27 15:22:44 -0300

    creando endpoint de PATCH para actualizar disponibilidad

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -66,3 +66,25 @@ export const updateProduct = async (req: Request, res: Response) => {
 
     res.json({data: product})
 }
+
+export const updateAvailability = async (req: Request, res: Response) => {
+
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) 
+    {
+        return res.status(404).json({
+            error: 'Producto No Encontrado'
+        })
+    }
+
+    //Actualizar
+
+    product.availability = !product.dataValues.availability
+    await product.save()
+
+    
+
+    res.json({data: product})
+}
