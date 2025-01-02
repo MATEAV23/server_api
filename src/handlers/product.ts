8702bdee00ed3d2dcd0ab4acffdc3cf36edec4b8
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-02 15:40:38 -0300

    haciendo el test coverage

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,22 +3,17 @@ import Product from '../models/Product.model'
 
 export const getProducts = async (req: Request, res: Response) => {
 
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['id', 'DESC']
-            ]
-        })
-        res.json({ data: products })
-    } catch (error) {
-        console.log(error)
-    }
+    const products = await Product.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    })
+    res.json({ data: products })
 }
 
 export const getProductById = async (req: Request, res: Response) => {
 
-    try {
-        const { id } = req.params
+    const { id } = req.params
         const product = await Product.findByPk(id)
 
         if (!product) 
@@ -28,19 +23,12 @@ export const getProductById = async (req: Request, res: Response) => {
             })
         }
         res.json({ data: product })
-    } catch (error) {
-        console.log(error)
-    }
 }
 
 export const createProduct = async (req: Request, res: Response) => {
 
-    try {
-        const product = await Product.create(req.body)
+    const product = await Product.create(req.body)
         res.status(201).json({ data: product })
-    } catch (error) {
-        console.log(error)
-    }
 
     //crear Producto
 
