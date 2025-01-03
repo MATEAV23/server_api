b057dee7b789792bb0dbdf162e337ee9bbd4bf67
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-02 23:44:37 -0300

    agregando css a la documentacion de swagger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import colors from 'colors'
 import swaggerUi from 'swagger-ui-express'
-import swaggerSpec from './config/swagger'
+import swaggerSpec, {swaggerUiOptions} from './config/swagger'
 import express from 'express'
 import router from './router'
 import db from './config/db'
@@ -29,7 +29,7 @@ server.use(express.json())
 server.use('/api/products', router)
 
 // Docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions))
 
 
 
